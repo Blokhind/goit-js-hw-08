4fc0b020a2c2948bc9cc2e9e7c911e6c1af086bf
Blokhind <113214742+Blokhind@users.noreply.github.com>
2023-02-10 14:26:41 +0200

    task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const markup = galleryItems
+  .map(
+    item => `
+<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>
+`
+  )
+  .join('');
+
+const galleryRef = document.querySelector('.gallery');
+galleryRef.insertAdjacentHTML('afterbegin', markup);
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,10 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', function ({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+});
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
